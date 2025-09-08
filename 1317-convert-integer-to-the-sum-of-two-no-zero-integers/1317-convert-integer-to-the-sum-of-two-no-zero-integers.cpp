1a251bc81bba03166610e5717b981b37d3e89d40
Akshay kumar <akshaykumarmaddemoni@gmail.com>
2025-09-08 16:58:35 +0530

    Time: 0 ms (100%), Space: 9.3 MB (13.18%) - LeetHub

---
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+         vector<int> arr;
+        
+         for(int i=1;i<=n/2;i++){
+           string  s1 = to_string(n-i);
+            
+            string  s2 = to_string(i);
+            if(s1.find('0') == string :: npos && s2.find('0') == string :: npos) {
+            arr.push_back(n-i);
+            arr.push_back(i);
+            return arr;
+            }
+         }
+         return arr;
+    }
+};
