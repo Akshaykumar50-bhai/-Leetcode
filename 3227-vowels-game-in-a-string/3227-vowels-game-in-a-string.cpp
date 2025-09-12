4f0b94eb3beed81a47fce4d35f792be176c174cf
Akshay kumar <akshaykumarmaddemoni@gmail.com>
2025-09-12 16:53:15 +0530

    Time: 19 ms (9.89%), Space: 18.9 MB (95.71%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    bool doesAliceWin(string s) {
-        string s1 ="aeiou";
+    bool doesAliceWin(string s){
         int cnt = 0;
         for(int i=0;i<s.length();i++){
            if(s[i]=='a' ||s[i]=='e'|| s[i]=='i'|| s[i]=='o'|| s[i]=='u' ){
@@ -9,7 +8,6 @@ public:
            }
         } 
         if(cnt == 0) return false;
-        if((cnt & 1) == 0) return true;
         return true; 
     }
 };
