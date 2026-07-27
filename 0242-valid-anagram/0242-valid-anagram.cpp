2a0730001493d58a01bc6df47faab6a223770377
Akshay kumar <akshaykumarmaddemoni@gmail.com>
2026-07-27 15:56:08 +0530

    Time: 0 ms (100%), Space: 10 MB (11.56%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      unordered_map<char,int> res;
+      
+      if(s.length() != t.length()) return false;
+       
+        for(char it : s ){
+            res[it] += 1;
+        }
+        for(char it: t){
+            
+            res[it] -=1;
+        }
+        for(const auto& i : res){
+              if(i.second != 0) return false;
+        }
+        
+        return true;
+    }
+};
