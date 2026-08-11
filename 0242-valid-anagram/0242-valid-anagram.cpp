3595a51e6ce09b05ad9396454c0e5b63fad1d1bd
Akshay kumar <akshaykumarmaddemoni@gmail.com>
2026-08-11 14:19:13 +0530

    Time: 1 ms (54.51%), Space: 9.7 MB (76.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
       unordered_map<char,int> res;
-      
       if(s.length() != t.length()) return false;
-       
         for(char it : s ){
             res[it] += 1;
         }
