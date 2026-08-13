397cf6c227926e4ff90dacc735ed767f0655402b
Akshay kumar <akshaykumarmaddemoni@gmail.com>
2026-08-14 00:23:33 +0530

    Time: 3 ms (75.88%), Space: 134.2 MB (98.92%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -5,23 +5,22 @@ public:
         vector<int>res(nums.size(),-1);
         if(nums.size()< (k+k+1)) return res;
         long long sum = 0,right = k+k;
-        int n = k;
+         
         for(int j=0;j<(k+k+1);j++){
                sum += nums[j];
             }
-            res[n] = sum/(k+k+1);
+            res[k] = sum/(k+k+1);
             right++;
-            n++;
+            
         
         int i = 1;
-        while(n<nums.size()-k){
+        while((k+i)<nums.size()-k){
                           
                sum += nums[right];
                sum -= nums[i-1];
                 
-               res[n] = sum/(k+k+1);
+               res[k+i] = sum/(k+k+1);
                right++;
-               n++;
                i++;
         }
         
