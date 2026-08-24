01391c76542015da573fc4273dfae54b381e707c
Akshay kumar <akshaykumarmaddemoni@gmail.com>
2026-08-24 22:04:51 +0530

    Time: 2 ms (28.92%), Space: 59.2 MB (70.87%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -5,9 +5,10 @@ public:
         int sum=0;
         for(int i=0;i<arr.size();i++){
             if(i-j < k) sum += arr[i];
-             if(i-j+1 == k){
-               int avg = sum/(i-j+1);
-               if(avg >= threshold) cnt++;
+            
+            if(i-j+1 == k){
+               
+               if(sum/k >= threshold) cnt++;
                sum -= arr[j];
                j++;
             }
