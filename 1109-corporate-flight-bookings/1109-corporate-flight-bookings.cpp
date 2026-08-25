59d890a7b3b076fd613b911043a818f4a647ccd4
Akshay kumar <akshaykumarmaddemoni@gmail.com>
2026-08-25 15:24:17 +0530

    Time: 279 ms (18.3%), Space: 72.8 MB (43.58%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -2,10 +2,29 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int>arr(n,0);
+       
+        vector<int>diff(n+1,0);
+         
         for(int i=0;i<bookings.size();i++){
-            for(int j = bookings[i][0]-1 ; j < bookings[i][1] ; j++){
-                arr[j] += bookings[i][2];
-            }
+            int l=bookings[i][0];
+            int r = bookings[i][1];
+            int val = bookings[i][2];
+            diff[l-1] += val;
+            diff[r] -= val;
+        }
+         for(int i=0;i<diff.size();i++){
+            cout << diff[i] << " ";
+         }
+          int v = 0;
+        for(int i=0;i<diff.size();i++){
+             if(i == 0) {diff[i] += 0;
+             v += diff[i];}
+             else{
+                v += diff[i];
+                diff[i] = v;
+             }
+             if(i < n) arr[i] += v;
+                       
         }
         return arr;
     }
