a0d9ce52600dd1123c17e95615d61daa3264e607
Akshay kumar <akshaykumarmaddemoni@gmail.com>
2026-09-19 13:05:24 +0530

    Time: 0 ms (100%), Space: 9 MB (7.92%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
+double val = 1; 
+    double pos(double x  , long long n ){
+        if(n == 0) return 1.0;
+        
+        if(n < 0){
+            return 1/pos(x ,-n);
+        }
+         double half = pow(x,n/2);
+         if(n%2 == 0) return half *half;
+         else return half*half*x;
+
+         return 0;
+        
+    }
     double myPow(double x, int n) {
-        double a = pow(x,n);
-        return a;
+     return pos(x , (long long)n);
     }
 };
